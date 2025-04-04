fd30ba7b812072a0427cbca53f77bf670eb6c90c
Miguel Angel <miguelonrs45@gmail.com>
2025-04-04 14:16:44 +0200

    Cambios de seguridad para autentificacion de la pagina web

--- TFG/config/supabase-config.js
+++ TFG/config/supabase-config.js
@@ -1,6 +1,4 @@
 export const supabaseConfig = {
-    // URL y clave de Supabase
-    // Cambiarlo a  la URL y clave de tu proyecto de Supabase de forma segura sin estar en el mismo codigo
-    supabaseUrl: 'https://dvtkkaxjehfotylwwsea.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2dGtrYXhqZWhmb3R5bHd3c2VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4MzEyOTUsImV4cCI6MjA1ODQwNzI5NX0.VjR1V7PBZuOtvSUIOnjrXBb_O-w6W2wabHhjahaom1A'
-  };
+  supabaseUrl: import.meta.env.VITE_SUPABASE_URL,
+  supabaseKey: import.meta.env.VITE_SUPABASE_KEY
+};
